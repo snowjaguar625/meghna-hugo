bf7887c4c49796a99e26f5b2ad84ec5babb55807
snowjaguar625 <snowjaguar625@gmail.com>
2020-10-19 08:56:57 +0600

    added netlify button

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,6 @@ $(window).on('load', function () {
 	$('.preloader').fadeOut(100);
 });
 
-// Auto close Navbar when click on link
-$('.navbar-collapse a').click(function(){
-	$(".navbar-collapse").collapse('hide');
-});
-
 jQuery(function ($) {
 	"use strict";
 
